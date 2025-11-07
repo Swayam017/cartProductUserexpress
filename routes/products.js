5acec3b69f8c656d845d05669f5cee05b839615a
Swayam017 <swayampravasahoo017@gmail.com>
2025-11-07 22:40:16 +0530

    Used controller

--- routes/products.js
+++ routes/products.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-// GET /products - Fetch all products
-router.get("/", (req, res) => {
-  res.send("Fetching all products");
-});
+const productController = require("../controllers/ProductController");
 
-// POST /products - Add a new product
-router.post("/", (req, res) => {
-  res.send("Adding a new product");
-});
+router.get("/", productController.getAllProducts);
+router.post("/", productController.addProduct);
+router.get("/:id", productController.getProductById);
 
-// GET /products/:id - Fetch product by ID
-router.get("/:id", (req, res) => {
-  res.send(`Fetching product with ID: ${req.params.id}`);
-});
 
 module.exports = router;
