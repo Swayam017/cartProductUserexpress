70807bda35597f317019fd40717474ea2f13fc36
Swayam017 <swayampravasahoo017@gmail.com>
2025-11-11 02:05:28 +0530

    Centralized Error handling

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,14 +1,23 @@
+const {sendErrorResponse,sendResponse} = require("../utils/response")
 const getAllUsers = (req, res) => {
     res.send("Fetching all users");
 };
 
 const addUser = (req, res) => {
-    res.send("Adding a new user");
+   const {name,email} = req.body;
+   if(!name || !email){
+    return sendErrorResponse(res,{message:"Name and Email are required",statusCode:400});
+   }
+   const user = {id:1,name,email};
+   return sendResponse(res,user,201)
 };
 
 const getUserById = (req, res) => {
-    const id = req.params.id;
-    res.send(`Fetching user with ID: ${id}`);
+    const user = req.params.id;
+    if(user > 10){
+        return sendErrorResponse(res,{message:"user not found",statusCode:404});
+    }
+    return sendResponse(res,user,200);
 };
 
 const updateUser = (req, res) => {
